ced80c03d836d89e3967e9bc5f6f5bd3522bf2a4
nacho <49342621+iheanachochukwu@users.noreply.github.com>
2024-10-19 20:01:25 +0100

    add tags

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -11,5 +11,4 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.subnet_address_space
-  tags = var.tags
 }
